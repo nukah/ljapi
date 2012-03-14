48c24059f8a3cdcf098c0410482fa90dbf332e00
Nuke <flow.energy@gmail.com>
2012-03-14 16:36:21 +0400

    attempt to fix json-pure error

--- lib/ljapi/post.rb
+++ lib/ljapi/post.rb
@@ -68,7 +68,7 @@ module LJAPI
       
       def run
         super
-        package = @result[:data]['events'].collect! { |post| post.each { |k,v| v.to_s.force_encoding('utf-8').encode }}
+        package = @result[:data]['events'].collect! { |post| post.each { |k,v| k.to_s; v.to_s.force_encoding('utf-8').encode }}
         return JSON.generate(@result)
       end
     end

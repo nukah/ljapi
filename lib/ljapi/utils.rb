b6fad86e3683a3fe150f0982f6981edff2bea871
Nuke <flow.energy@gmail.com>
2012-05-29 14:41:17 +0400

    апдейт для возможности выгрузки по lastsync

--- lib/ljapi/utils.rb
+++ lib/ljapi/utils.rb
@@ -1,6 +1,7 @@
+
 module Utils
   def self.time_to_ljtime(time)
-    time.strftime '%Y-%m-%d %H:%M:%S'
+    DateTime.parse(time).strftime '%Y-%m-%d %H:%M:%S'
   end
   def self.ljtime_to_time(str)
     dt = DateTime.strptime(str, '%Y-%m-%d %H:%M')
